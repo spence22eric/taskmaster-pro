9d0f95706ab49931cb028c6b8ebe7a99f855cb52
Eric Spencer <spencer22eric@gmail.com>
2022-03-06 09:26:51 -0500

    add sortable feature and droppable to trash zone to delete tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,7 +132,7 @@ $(".list-group").on("blur", "input[type='text']", function () {
   tasks[status][index].date = date;
   saveTasks();
 
-  // recreate span element with boostrap classes
+  // recreate span element with bootstrap classes
   let taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(date);
@@ -183,6 +183,78 @@ $("#remove-tasks").on("click", function () {
     $("#list-" + key).empty();
   }
   saveTasks();
+});
+
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+
+  activate: function (event) {
+    console.log("activate", this);
+  },
+
+  deactivate: function (event) {
+    console.log("deactivate", this);
+  },
+
+  over: function (event) {
+    console.log("over", event.target);
+  },
+
+  out: function (event) {
+    console.log("out", event.target);
+  },
+
+  
+
+  update: function (event) {
+    // array to store the task data in
+    let tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this).children().each(function () {
+      let text = $(this)
+        .find("p")
+        .text()
+        .trim();
+
+      let date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      // add task data to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      });
+    });
+    // trim down list's ID to match object property
+    let arrName = $(this)
+    .attr("id")
+    .replace("list-", "");
+
+    // update array on tasks object and save
+    tasks[arrName] = tempArr;
+    saveTasks();
+  }
+});
+
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    console.log("drop");
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
 });
 
 // load tasks for the first time
